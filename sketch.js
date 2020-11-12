4e4a8e129a75481b2bf9b1f5a071138fa66cf260
Ayan Poddar <53509045+ovpoddar@users.noreply.github.com>
2020-11-12 16:43:10 +0530

    Type o

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var bubbls = [],
 
 
 function setup() {
-    createCanvas(683, Window.innerHeight);
+    createCanvas(683, window.innerHeight);
     for (let i = 0; i < chances; i++) {
         var index = round(random(2));
         shootingbubble.push(new MovingBubble(width / 2, height, colors[index])); 
